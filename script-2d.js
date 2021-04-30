a80ae5edcc62f6633ff58db505bf4171cc66094d
Jon <pokemonmasterjmw@gmail.com>
2021-04-30 19:59:34 -0400

    Update script-2d.js

--- script-2d.js
+++ script-2d.js
@@ -364,23 +364,24 @@ function init(){
 //            return false
 //       },
         submit : function(event){
+		    //var stripe = Stripe('pk_live_51IbUhkHy8pZ91dsyEHbItdV3dRUHfxAhBaBYaYQvVrofC3IoygYQcjbEaMUcDhaaWYOvCU30o3zm0hS5mVLZZBQi00nfYUtQmb'); // Prod
+	        var stripe = Stripe('pk_test_51IbUhkHy8pZ91dsyNfbUFA1ynj6Sb0NmifdoQm4ISo83X4cOFpA68UH0DbLrgzsaQxlV3lJrGr394Cj3GMCUHTcA006LK2wa7Y'); //TODO: change to Dwellito
+
+		    //const priceID = 'price_1IiUe4Hy8pZ91dsyzSVEk4at'; // TODO: get dynamically from Webflow OR get hard coded from Anil/Caleb PROD
+			const priceID = 'price_1IjTR7Hy8pZ91dsytU0x1YAD'; // TODO: get dynamically from Webflow OR get hard coded from Anil/Caleb
+		
             var data = $('form').serialize()
 			//console.log(data)
             data = window.btoa(data)
             var sTags = JSON.stringify(this.studioItems)
 			//console.log(sTags)
             var t = window.btoa(sTags)
-			
-			//var stripe = Stripe('pk_live_51IbUhkHy8pZ91dsyEHbItdV3dRUHfxAhBaBYaYQvVrofC3IoygYQcjbEaMUcDhaaWYOvCU30o3zm0hS5mVLZZBQi00nfYUtQmb'); // Prod
-	        var stripe = Stripe('pk_test_51IbUhkHy8pZ91dsyNfbUFA1ynj6Sb0NmifdoQm4ISo83X4cOFpA68UH0DbLrgzsaQxlV3lJrGr394Cj3GMCUHTcA006LK2wa7Y'); //TODO: change to Dwellito
-
-			//const priceID = 'price_1IiUe4Hy8pZ91dsyzSVEk4at'; // TODO: get dynamically from Webflow OR get hard coded from Anil/Caleb PROD
-			const priceID = 'price_1IjTR7Hy8pZ91dsytU0x1YAD'; // TODO: get dynamically from Webflow OR get hard coded from Anil/Caleb
 		
 			var successURL = "https://" + window.location.hostname + "/thank-you?s=" + data + "&t=" + t
 			var cancelURL = "https://" + window.location.hostname; //TODO: Finish
-            //console.log(successURL)
-			return stripe.redirectToCheckout({
+            console.log(successURL)
+			console.log(stripe)
+			stripe.redirectToCheckout({
 				lineItems: [{price: priceID, quantity: 1}],
 				mode: 'payment',
 				/*
@@ -404,6 +405,7 @@ function init(){
                     console.log(result.error.message)
                 }
             });
+			return false;
        },
         changeCurrency : function(c){
             this.currency = c
