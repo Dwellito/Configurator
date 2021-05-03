5968c0be4c0129422e2d3163d01e1c7f912ada77
Jon <pokemonmasterjmw@gmail.com>
2021-05-03 19:48:30 -0400

    Update script-2d.js

--- script-2d.js
+++ script-2d.js
@@ -390,7 +390,7 @@ function init(){
 				 */
 				successUrl: successURL,
 				cancelUrl: cancelURL,
-				customerEmail: document.getElementById("email-input").value;,
+				customerEmail: document.getElementById("email-input").value,
 			})
             .then(function (result) {
                 if (result.error) {
