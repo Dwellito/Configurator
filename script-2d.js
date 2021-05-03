46f05e210e43c23e54d7f44633d1f22fb77ba1b7
Jon <pokemonmasterjmw@gmail.com>
2021-05-03 19:50:52 -0400

    Update script-2d.js

--- script-2d.js
+++ script-2d.js
@@ -364,10 +364,10 @@ function init(){
 //            return false
 //       },
         submit : function(event){
-		    //var stripe = Stripe('pk_live_51IbUhkHy8pZ91dsyEHbItdV3dRUHfxAhBaBYaYQvVrofC3IoygYQcjbEaMUcDhaaWYOvCU30o3zm0hS5mVLZZBQi00nfYUtQmb'); // Prod
+            //var stripe = Stripe('pk_live_51IbUhkHy8pZ91dsyEHbItdV3dRUHfxAhBaBYaYQvVrofC3IoygYQcjbEaMUcDhaaWYOvCU30o3zm0hS5mVLZZBQi00nfYUtQmb'); // Prod
 	    var stripe = Stripe('pk_test_51IbUhkHy8pZ91dsyNfbUFA1ynj6Sb0NmifdoQm4ISo83X4cOFpA68UH0DbLrgzsaQxlV3lJrGr394Cj3GMCUHTcA006LK2wa7Y'); //TODO: change to Dwellito
 
-		    //var priceID = 'price_1IiUe4Hy8pZ91dsyzSVEk4at'; // TODO: get dynamically from Webflow OR get hard coded from Anil/Caleb PROD
+	    //var priceID = 'price_1IiUe4Hy8pZ91dsyzSVEk4at'; // TODO: get dynamically from Webflow OR get hard coded from Anil/Caleb PROD
             var priceID = 'price_1IjTR7Hy8pZ91dsytU0x1YAD'; // TODO: get dynamically from Webflow OR get hard coded from Anil/Caleb
 		
             //var data = $('form').serialize()
@@ -377,7 +377,14 @@ function init(){
 		
 			var successURL = "https://" + window.location.hostname + "/thank-you"
 			var cancelURL = "https://" + window.location.hostname + "/payment-failure"; //TODO: Finish
-
+			var emailElement = document.getElementById("email-input");
+		        
+		        var email = null;
+		
+		        if (emailElement) {
+		          email = emailElement.value
+			}
+	
 			stripe.redirectToCheckout({
 				lineItems: [{price: priceID, quantity: 1}],
 				mode: 'payment',
