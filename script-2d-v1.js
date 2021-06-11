7ad31b8093fcb20ee24390725f567c885a01f218
QuoAPIs8 <72159813+QuoAPIs8@users.noreply.github.com>
2021-06-11 18:32:59 -0500

    Add files via upload

--- script-2d-v1.js
+++ script-2d-v1.js
@@ -309,7 +309,7 @@ function init(){
                             if(i.slug == slug) i.active = !i.active
                             return i
                         })
-                        if(item.childs.length > 0){
+                        if(item.childs.length > 0 && !item.active){
                             item.childs.map(function(c){
                                 return c.active = false
                             })
