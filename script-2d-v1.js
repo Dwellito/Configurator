d158893cbd04f0288176fb3f10e5638b9e354b96
QuoAPIs8 <72159813+QuoAPIs8@users.noreply.github.com>
2021-06-11 18:28:00 -0500

    Add files via upload

--- script-2d-v1.js
+++ script-2d-v1.js
@@ -309,11 +309,11 @@ function init(){
                             if(i.slug == slug) i.active = !i.active
                             return i
                         })
-                        if(item.childs.length > 0 && !item.active){
-                            item.childs.map(function(c){
-                                return c.active = false
-                            })
-                        }
+                    }
+                    if(item.childs.length > 0 && !item.active){
+                        item.childs.map(function(c){
+                            return c.active = false
+                        })
                     }
 
                 }else if(item.selection == "simple"){
@@ -339,7 +339,7 @@ function init(){
                     levels : []
                 }
 
-                if(item.childs.length > 0 && item.active){
+                if(item.childs.length > 0 ){
                     this.activeOptionLevel = {
                         slug : item.slug,
                         levels : [levels[item.selection ][0]]
