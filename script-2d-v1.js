19aa35c52111e586f408d843cdd7e29fc4e8db1f
jonwalch <jonwalch@gmail.com>
2021-06-18 14:52:18 -0400

    Test getting stripe secret from backend 8

--- script-2d-v1.js
+++ script-2d-v1.js
@@ -550,45 +550,44 @@ function init(){
             this.studio.load = formatter.format(total_porcentage)+"/mo"
         },
         goSlide : function(slide) {
-            console.log("go slide top")
             if (slide == 'next'){ slide = (this.valid) ? parseInt(this.slideActive) + 1 : this.slideActive }
             this.valid = true
             var inputs = $("input:required").filter(function(i, elem){
                 return $(elem).val() == ""
             })
-            console.log("go slide before if")
             if (slide == this.summarySlide){
                 if (inputs.length > 0){
                     this.valid = false
-                    console.log("if")
                 }
                 else {
                     this.valid = true
                     const emailElement = document.getElementById("Email");
                     const email = emailElement.value;
-                    console.log("else")
                     console.log(email)
                     console.log(getModelName(window.location.pathname))
                     this.setPrice()
-                    // if (stripePaymentIntentSecret === null) {
-                    //     var response = fetch('https://cede9a7b9b21.ngrok.io/api/stripe/secret', {
-                    //         method : "POST",
-                    //         mode: "cors",
-                    //         redirect: "error",
-                    //         body: JSON.stringify({
-                    //             amount: 30000,
-                    //             email: email,
-                    //             model: getModelName(window.location.pathname)
-                    //         })
-                    //     }).then(function(response) {
-                    //         return response.json();
-                    //     }).then(function(responseJson) {
-                    //         console.log(responseJson)
-                    //         stripePaymentIntentSecret = responseJson.secret;
-                    //         // Render the form to collect payment details, then
-                    //         // call stripe.confirmCardPayment() with the client secret.
-                    //     });
-                    // }
+                    if (stripePaymentIntentSecret === null) {
+                        var response = fetch('https://cede9a7b9b21.ngrok.io/api/stripe/secret', {
+                            method : "POST",
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            mode: "cors",
+                            redirect: "error",
+                            body: JSON.stringify({
+                                amount: 30000,
+                                email: email,
+                                model: getModelName(window.location.pathname)
+                            })
+                        }).then(function(response) {
+                            return response.json();
+                        }).then(function(responseJson) {
+                            console.log(responseJson)
+                            stripePaymentIntentSecret = responseJson.secret;
+                            // Render the form to collect payment details, then
+                            // call stripe.confirmCardPayment() with the client secret.
+                        });
+                    }
                 }
             }
             if (this.valid) { $("#slick-slide-control0"+slide).click() }
