f3d91f4264fe8daf5ce348afe6c2902d21d45b67
jonwalch <jonwalch@gmail.com>
2021-07-23 10:46:52 -0400

    Add intercom to page in test

--- script-2d-v1.js
+++ script-2d-v1.js
@@ -236,6 +236,13 @@ function stripeMakePayment (card, secret) {
     });
 }
 
+function loadIntercom(){
+    window.intercomSettings = {
+        app_id: "wuhofi95"
+    };
+    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/' + APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+}
+
 $(() => {
     // View events based on the model and the builder. The verbosity is for funnel analysis limitations
     if (isProd()) {
@@ -252,6 +259,8 @@ $(() => {
             })
         }
     }
+    loadIntercom()
+
     // Minio hotjar user tracking
     if (modelIsMinio() && isProd()) {
         (function(h,o,t,j,a,r){
