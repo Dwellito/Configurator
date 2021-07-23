3bebd014fe98dd7c4fc1a6a7454e91225fe40f44
jonwalch <jonwalch@gmail.com>
2021-07-23 14:35:43 -0400

    Intercom in prod

--- script-2d.js
+++ script-2d.js
@@ -90,6 +90,7 @@ var stripePaymentIntentSecret = null;
 var stripePaymentIntentID = null;
 var stripeCard = null;
 var stripeObj = null;
+var intercomAdded = false;
 
 const redirectToStripe = function() {};
 
@@ -238,6 +239,14 @@ function stripeMakePayment (card, secret) {
             }
         }
     });
+}
+
+function loadIntercom(){
+    const APP_ID = "wuhofi95"
+    window.intercomSettings = {
+        app_id: APP_ID
+    };
+    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/' + APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 }
 
 $(() => {
@@ -883,6 +892,12 @@ function init(){
 
             if (slideName === "size") {
                 slideName = "model"
+            }
+
+            if (slideName === "layout" && !intercomAdded){
+                loadIntercom()
+                intercomAdded = true;
+                Intercom('showNewMessage');
             }
 
             if (isProd()) {
