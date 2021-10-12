1b038a9e0a5c872b0b62fb19a0bb0aef7311e5d1
jonwalch <jonwalch@gmail.com>
2021-10-12 12:32:15 -0400

    Second stab front end script for calculator

--- calc.js
+++ calc.js
@@ -6,42 +6,83 @@ const backendUrl = isProd() ? "https://dwellito.co" : "https://test.dwellito.co"
 
 const getModelName = thePath => thePath.substring(thePath.lastIndexOf('/') + 1)
 
-async function submitCalc () {
-    //TODO: the rest of the fields
-    // const city = document.getElementById('City').value.trim();
+function currencyToNumber (str) {
+    const num = Number(str.replace(/[^0-9\.-]+/g,""))
+    return isNaN(num) ? -1 : num
+}
 
-    if (true) { // no empty fields -> implies they're in the right spot in the flow to submit
-        const response = await fetch(backendUrl + '/api/calc', {
-            method : "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            mode: "cors",
-            redirect: "error",
-            body: JSON.stringify({
-                "loan-amount": 100000,
-                email: "ron@jon.com",
-                model: getModelName(window.location.pathname),
-                name: "Namington Namer",
-                address: "12345 Senses Working Overtime",
-                city: "There",
-                zipcode: "12345",
-                state: "NC",
-                phone: "1234567890",
-                "credit-score": "780", //string
-                "household-income": 100000, //int
-                "monthly-payment": 1457, //int
-                "down-payment": 0.1, //decimal
-                "interest-rate": 0.04, //decimal
-                term: 30, //int
-                dob: "10/22/1980", //string
-                debt: 150000 //int
-            })
-        })
-        const responseJson = await response.json()
+async function submitCalc () {
+    const fourAPR = document.getElementById("4 APR").checked
+    const fiveAPR = document.getElementById("5 APR").checked
+    const sixAPR = document.getElementById("6 APR").checked
+
+    var monthly;
+    var interestRate;
+    var down;
+
+    if (fourAPR) {
+        monthly = "20-down-price"
+        interestRate = 0.04
+        down = 0.2
+    } else if (fiveAPR) {
+        monthly = "10-down-price"
+        interestRate = 0.05
+        down = 0.1
+    } else {
+        monthly = "no-down-price"
+        interestRate = 0.06
+        down = 0
     }
+
+    const amount = document.getElementById("Amount").value || document.getElementById("Amount").placeholder
+    const loanAmount = currencyToNumber(amount);
+
+    const monthlyPaymentStr = document.getElementById(monthly).innerHTML
+    const monthlyPayment = currencyToNumber(monthlyPaymentStr.substring(0, monthlyPaymentStr.length - 3))
+
+    const dob = document.getElementById("Date-of-birth").value
+    const creditScore = document.getElementById("Credit-score").value
+    const householdIncome = currencyToNumber(document.getElementById("Household-Income").value)
+    const debt = currencyToNumber(document.getElementById("Debt").value)
+
+    const name = document.getElementById("Full-Name-2").value
+    const phone = document.getElementById("Phone-Number-3").value
+    const email = document.getElementById("Email-2").value
+    const address = document.getElementById("Address-2").value
+    const city = document.getElementById("City-2").value
+    const state = document.getElementById("State-2").value
+    const zip = document.getElementById("Zip-Code-2").value
+
+    const response = await fetch(backendUrl + '/api/calc', {
+        method : "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        mode: "cors",
+        redirect: "error",
+        body: JSON.stringify({
+            "loan-amount": loanAmount,
+            email: email,
+            model: getModelName(window.location.pathname),
+            name: name,
+            address: address,
+            city: city,
+            zipcode: zip,
+            state: state,
+            phone: phone,
+            "credit-score": creditScore, //string
+            "household-income": householdIncome, //int
+            "monthly-payment": monthlyPayment, //int
+            "down-payment": down, //decimal
+            "interest-rate": interestRate, //decimal
+            term: 30, //int
+            dob: dob, //string
+            debt: debt //int
+        })
+    })
+    const responseJson = await response.json()
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("Continue").addEventListener("click", submitCalc)
+    document.getElementById("Complete-Submission").addEventListener("click", submitCalc)
 }, false);
